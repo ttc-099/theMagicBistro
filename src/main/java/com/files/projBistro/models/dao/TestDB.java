2af6a07fb5edd55661cf536ade52a8b760146269
ttc-099 <sherianne.sactt@gmail.com>
2026-04-27 05:29:06 +0800

    fixed misc. bugs

--- src/main/java/com/files/projBistro/models/dao/TestDB.java
+++ src/main/java/com/files/projBistro/models/dao/TestDB.java
@@ -6,7 +6,6 @@ import java.sql.DriverManager;
 public class TestDB {
     public static void main(String[] args) {
         try {
-            // This line checks if the Driver is in your library
             Class.forName("org.sqlite.JDBC");
             Connection conn = DriverManager.getConnection("jdbc:sqlite:bistroTrue.db");
             System.out.println("✅ DATABASE CONNECTION SUCCESSFUL!");
